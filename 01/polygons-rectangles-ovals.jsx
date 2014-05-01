d8dd2bdc643fc540d13e527edc2bdec36b4a9922
fabiantheblind <icke@fabianmoronzirfas.me>
2014-05-01 10:30:31 +0200

    some test for path notation for @sebastian-meier

--- 01/polygons-rectangles-ovals.jsx
+++ 01/polygons-rectangles-ovals.jsx
@@ -2,6 +2,7 @@
     * Rectangles ovals polygons
     */
     var  main = function(){
+    var debug = true;
     var pw = 150; // for better handling
     var ph = pw/3; // also
 
@@ -42,19 +43,34 @@
     var items = [rect,oval,poly];
 
 
+    var path = "";
+
      if($.os.charAt(0) == 'W'){
-            alert("You are working on windows. It could be that the path notation used in this script won't work. If so copy the content of this panel into an new issue at 'https://github.com/fabiantheblind/extendscript-101'\n\n" +
+           if(debug) alert("You are working on windows. It could be that the path notation used in this script won't work. If so copy the content of this panel into an new issue at 'https://github.com/fabiantheblind/extendscript-101'\n\n" +
+                "OS: " + $.os + "\n\n"+
                 "Path for this script is: " + ((File($.fileName)).path) + "\n\n" +
                 "Path we want to adress: " + ((File($.fileName)).parent.path) + "/assets/images/" + "\n"
              );
-        }else{
+            path = ((File($.fileName)).parent.path) + "\\assets\\images\\";
+
+        }else if($.os.charAt(0) == 'M'){
             // nothing to do here move along
+            path = ((File($.fileName)).parent.path) + "/assets/images/";
+        } else {
+
+            if(debug) alert("Uh? No Windows nor Mac? What Magick is this?\n"+
+                "copy the content of this panel into an new issue at 'https://github.com/fabiantheblind/extendscript-101'\n\n"+
+                    "OS: " + $.os + "\n\n"+
+                    "Path for this script is: " + ((File($.fileName)).path) + "\n\n" +
+                "Path we want to adress: " + ((File($.fileName)).parent.path) + "/assets/images/" + "\n"
+                );
+            path = ((File($.fileName)).parent.path) + "/assets/images/";
+
         }
 
     // get the path of the scriptfile
     // the files we are going to load get found via their relativ path
     // needs testing on windows
-    var path = ((File($.fileName)).parent.path) + "/assets/images/";
     // load 3 images provided with the repo
     // /assets/images/feet.jpg
     // /assets/images/frog.jpg
@@ -68,7 +84,7 @@
         try {
         thing.place(File(images[i])); // place one of the images
         }catch(e){
-            alert("I could not find the images. could be that they are gone or they are at ");
+            if(debug) alert("I could not find an image. Could it be that they are gone? I am looking for it @ \n" + File(images[i]).path);
         }
         thing.fit(FitOptions.CENTER_CONTENT); // fit it
         thing.fit(FitOptions.FILL_PROPORTIONALLY); // fit it
